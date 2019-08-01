14cdc539d4a7e3c5a10d987951173ff4824b31e1
Chantel Lowe <chantellowe17@gmail.com>
2019-08-01 04:09:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@
 #end
 
 def reverse_each_word(array)
-  new_arr = array.split(" ")
-  new_arr.collect { |sentence| sentence.reverse!}
-  new_arr.join(" ")
+  array.split(" ")
+  array.collect { |sentence| sentence.reverse!}
+  array.join(" ")
 end
